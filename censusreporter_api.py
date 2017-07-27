b4a0efb2b4e056be4e50941b66370b7f8613fe86
Joe Germuska <joe@germuska.com>
2017-07-27 11:34:50 -0500

    upgrade to Python 3 for notebooks. Make minor efforts to stay compatible with Python 2

--- censusreporter_api.py
+++ censusreporter_api.py
@@ -1,12 +1,15 @@
 # set up some utility methods. If we ever make a python API wrapper, these belong there.
 import requests
 import pandas as pd
+from six import string_types
 
 API_URL="http://api.censusreporter.org/1.0/data/show/{release}?table_ids={table_ids}&geo_ids={geoids}"
+SEARCH_API_URL="http://api.censusreporter.org/2.1/full-text/search?type=profile&q={query}"
+
 def _clean_list_arg(arg,default):
     if arg is None:
         arg = default
-    if isinstance(arg,basestring):
+    if isinstance(arg,string_types):
         arg = [arg]
     return arg
 
@@ -23,14 +26,14 @@ def json_data(tables=None, geoids=None, release='latest'):
     """
     geoids = _clean_list_arg(geoids,'040|01000US')
     tables = _clean_list_arg(tables,'B01001')
- 
-    url = API_URL.format(table_ids=','.join(tables).upper(), 
-                         geoids=','.join(geoids), 
+
+    url = API_URL.format(table_ids=','.join(tables).upper(),
+                         geoids=','.join(geoids),
                          release=release)
 
     response = requests.get(url)
     return response.json()
- 
+
 def _prep_data_for_pandas(json_data,include_moe=False):
     """Given a dict of dicts as they come from a Census Reporter API call, set it up to be amenable to pandas.DataFrame.from_dict"""
     result = {}
@@ -46,7 +49,7 @@ def _prep_data_for_pandas(json_data,include_moe=False):
         result[geoid] = flat
     return result
 
-def _prep_headers_for_pandas(json_data,separator=":", level=None):
+def _prep_headers_for_pandas(json_data,separator=":", level=None, include_moe=False):
     headers = {}
     for table in json_data['tables']:
         stack = [ None ] * 10 # pretty sure no columns are nested deeper than this.
@@ -63,9 +66,24 @@ def _prep_headers_for_pandas(json_data,separator=":", level=None):
                 name = separator.join(parts)
             if level is None or indent <= level:
                 headers[column] = name
+                if include_moe:
+                    moe_col = '{}_moe'.format(column)
+                    headers[moe_col] = "{} (error)".format(name)
     return headers
 
-def get_dataframe(tables=None, geoids=None, release='latest',level=None,place_names=True,column_names=True):
+def search_places(q,sumlevel=None,sumlevels=None):
+    url = SEARCH_API_URL.format(query=q)
+    resp = requests.get(url)
+    j = resp.json()
+    if sumlevel is not None:
+        sumlevels = [ sumlevel ]
+    if sumlevels is not None:
+        sumlevels = map(str,sumlevels)
+        return map(lambda x: x['sumlevel'] in sumlevels, j['results'])
+    else:
+        return j['results']
+
+def get_dataframe(tables=None, geoids=None, release='latest',level=None,place_names=True,column_names=True, include_moe=False):
     """Return a pandas DataFrame object for the given tables and geoids.
 
     Keyword arguments (all optional):
@@ -79,20 +97,21 @@ def get_dataframe(tables=None, geoids=None, release='latest',level=None,place_na
     level -- if provided, should be an integer representing the maximum "indent level" of columns to be returned. Generally, '0' is the total column.
     place_names -- specify False to omit a 'name' column for each geography row
     column_names -- specify False to preserve the coded column names instead of using verbal labels
+    include_moe -- specify True to include error columns. Defaults to false.
 
     """
     response = json_data(tables, geoids, release)
     if 'error' in response:
         raise Exception(response['error'])
-    df = pd.DataFrame.from_dict(_prep_data_for_pandas(response),orient='index')
+    df = pd.DataFrame.from_dict(_prep_data_for_pandas(response, include_moe=include_moe),orient='index')
     df = df.reindex_axis(sorted(df.columns), axis=1)
     if column_names or level is not None:
-        headers = _prep_headers_for_pandas(response, level=level)
+        headers = _prep_headers_for_pandas(response, level=level, include_moe=include_moe)
         if level is not None:
             df = df.select(lambda x: x in headers,axis=1)
         if column_names:
             df = df.rename(columns=headers)
     if place_names:
         name_frame = pd.DataFrame.from_dict(response['geography'],orient='index')
-        df.insert(0, 'name', name_frame.name) 
+        df.insert(0, 'name', name_frame.name)
     return df
